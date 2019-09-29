f66f73be5de94b4e3a6bdd90ed504fe56a77c817
openbsa <55908205+openbsa@users.noreply.github.com>
2019-09-29 15:54:40 +0530

    Add files via upload

---
+++ precache-manifest.baaaf8443adb93b057b8347d1f3f4e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956dbe01f64c6ec8395f6d720ac103e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cb49337d8cd03938b51",
+    "url": "/static/css/main.982abf81.chunk.css"
+  },
+  {
+    "revision": "0a3a2168ddb716936a0b",
+    "url": "/static/js/2.678c9f12.chunk.js"
+  },
+  {
+    "revision": "9cb49337d8cd03938b51",
+    "url": "/static/js/main.53c1cfa4.chunk.js"
+  },
+  {
+    "revision": "5bb9cb2c0ffb1b51dc96",
+    "url": "/static/js/runtime-main.ef68aeac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
